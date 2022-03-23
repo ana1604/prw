c4f884dea97f85d0cff62f7fb7821a9087b2c018
ana maria <clementino.ana15@gmail.com>
2022-03-23 11:08:28 -0300

    exer2 condicional

--- aula5/ex1.php
+++ aula5/ex1.php
@@ -1,14 +1,8 @@
 <?php
-    $n1=5;
-    $n2=3;
-    $media = ($n1 +$n2)/2;
-    
-    if($media>= 7)
-        echo "Você foi aprovado";
-    
-    else 
-        echo "você foi reprovado";
+    $n1 = 7;
+    $n2 = 8;
+    $media = ($n1 + $n2)/2;
 
-    
-
+    if($media >= 7)
+         echo "Você foi aprovado";
 ?>
