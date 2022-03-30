7aa7a10ac87ee257d32b032c7d5ee3ce068826a0
ana maria <clementino.ana15@gmail.com>
2022-03-30 11:02:00 -0300

    i

---
+++ aula6/cadastro/cadastro.php
@@ -0,0 +1,8 @@
+<?php
+   echo  "<h1> Cadastro PHP </h1> " ;
+   $nome = $_GET [ 'nome' ];
+   $e-mail = $_GET [ 'email' ];
+
+   echo  'Nome: ' . $nome . '<br>' ;
+   echo  'E-mail: ' .$e-mail . '<br>' ;
+?>
