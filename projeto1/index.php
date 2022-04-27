ac6af82e59dc7122924dc9a00617723abf799759
ana maria <clementino.ana15@gmail.com>
2022-04-27 11:23:52 -0300

    altera usuario

---
+++ projeto1/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Princiapal</title>
+    <link rel="stylesheet" href="estilo.css">
+</head>
+<body>
+    <h1>Menu Principal</h1>
+    <hr>
+    <div id="menu">
+        <ul>
+            <li><a hrfe="cadastro_usuario.html">Cadastrar usuario</a></li>
+            <li><a hrfe="listar_usuarios.php">Listar Usuárioa</a></li>
+        </ul>
+
+    </div>
+</hr>
+    
+</body>
+</html>
